0a53a65b4f226c9f44a05e280dc09cf70e377079
Ayush Acharjya <ayushacharjya@gmail.com>
2024-08-21 12:43:12 +0100

    create worker which will pick msgs from kafka

---
+++ apps/worker/src/index.ts
@@ -0,0 +1,36 @@
+import prisma from "@repo/db/client";
+import kafkaClient from "@repo/kafka/client";
+
+const TOPIC_NAME = "zap-events";
+
+async function main() {
+  const consumer = kafkaClient.consumer({
+    groupId: "main-worker",
+  });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+
+  await consumer.run({
+    autoCommit: false,
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value?.toString(),
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      console.log("Processing done");
+      await consumer.commitOffsets([
+        {
+          topic: TOPIC_NAME,
+          partition: partition,
+          offset: (parseInt(message.offset) + 1).toString(),
+        },
+      ]);
+    },
+  });
+}
+
+main();
